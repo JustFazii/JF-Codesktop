671b2e8a9eff422310005d2ea11006840f938a48
JustFazii <MJTrawkowski@outlook.com>
2023-02-06 18:39:06 +0100

    Fixed Detail, added Profiles sites, added CSS to profiles Sites

--- Codesktop/Controllers/ProfileController.cs
+++ Codesktop/Controllers/ProfileController.cs
@@ -12,7 +12,10 @@ namespace Codesktop.Controllers
         private readonly IApplicationUser _userService;
         private readonly IUpload _uploadService;
 
-        public ProfileController(UserManager<ApplicationUser> userManager, IApplicationUser userService, IUpload uploadService)
+        public ProfileController(
+            UserManager<ApplicationUser> userManager,
+            IApplicationUser userService,
+            IUpload uploadService)
         {
             _userManager = userManager;
             _userService = userService;
